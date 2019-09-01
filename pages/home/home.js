9f293c230233fc6a250d2956366a94170576aaa4
j376787348 <j376787348@163.com>
2019-09-01 15:09:59 +0800

    1.数据绑定,2.列表渲染,3.事件监听

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,62 +5,96 @@ Page({
    * 页面的初始数据
    */
   data: {
+    name: 'world',
+    students: [{
+        id: 1,
+        name: 'Tom',
+        age: 21
+      },
+      {
+        id: 2,
+        name: 'Tom2',
+        age: 22
+      },
+      {
+        id: 3,
+        name: 'Tom3',
+        age: 23
+      },
+      {
+        id: 4,
+        name: 'Tom4',
+        age: 24
+      },
+    ],
+    counter: 0
+  },
 
+  handleBtnClick() {
+    this.setData({
+      counter: this.data.counter + 1
+    })
+  },
+
+  handleBtnSubtraction() {
+    this.setData({
+      counter: this.data.counter - 1
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
