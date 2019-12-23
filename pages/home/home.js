ff97f53688ce3ceb71df70feaf2922dad642a3b8
syl <yongliang.su.wh@nikoyo.com.cn>
2019-12-23 11:00:52 +0800

    05_自定义相机1

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,7 +5,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    path: '../../assets/image/welcome.png',
+    isVisible: true
   },
 
   /**
@@ -62,5 +63,53 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+
+  takePhoto() {
+    const ctx = wx.createCameraContext()
+    ctx.takePhoto({
+      quality: 'high',
+      success: (res) => {
+        console.log('takePhoto---success===', res);
+        var path = res.tempImagePath;
+        this.setData({
+          path: path,
+          isVisible: false
+        })
+      }
+    })
+  },
+  error(e) {
+    console.log(e.detail)
+  },
+
+  submit() {
+    console.log('提交----');
+  },
+
+  retakePic() {
+    console.log('重拍---');
+    this.setData({
+      isVisible: true
+    })
+  },
+
+  preview(e) {
+    console.log('preview---e===', e);
+    var path = e.currentTarget.dataset.path;
+    wx.previewImage({
+      current: path,
+      urls: [path],
+      success: function (res) {
+        console.log('previewImage---success===', res);
+      },
+      fail: function (res) {
+        console.log('previewImage---fail===', res);
+      },
+      complete: function (res) {
+        console.log('previewImage---complete===', res);
+      },
+    })
   }
 })
